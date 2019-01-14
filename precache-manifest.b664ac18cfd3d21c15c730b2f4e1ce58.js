9b93ff2e30bc5bfb9f377f2de062b1b9b387f0f8
Ashish Kumar Tewari <ashishtewaripro@gmail.com>
2019-01-14 05:50:39 +0530

    Updates

---
+++ precache-manifest.b664ac18cfd3d21c15c730b2f4e1ce58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ae41ef7adf29b887855a806d72ebc7e",
+    "url": "/public-coffee/static/media/machine.3ae41ef7.png"
+  },
+  {
+    "revision": "4c92701675cfe4bede6e",
+    "url": "/public-coffee/static/js/runtime~main.4c927016.js"
+  },
+  {
+    "revision": "5eb4cdfd56b584897c49",
+    "url": "/public-coffee/static/js/main.5eb4cdfd.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/public-coffee/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "5eb4cdfd56b584897c49",
+    "url": "/public-coffee/static/css/main.fb6135e3.chunk.css"
+  },
+  {
+    "revision": "94565bb248b7c5f702b71f8a15b3e15c",
+    "url": "/public-coffee/index.html"
+  }
+];
